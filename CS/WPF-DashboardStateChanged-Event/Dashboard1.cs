11f4b97d9eead225b9ad283bb1181eec07c772ab
zakhodiaeva.margarita <margarita.zakhodiaeva@devexpress.com>
2020-04-08 21:30:25 +0300

    add cs solution

---
+++ CS/WPF-DashboardStateChanged-Event/Dashboard1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess;
+using DevExpress.DashboardCommon;
+
+namespace WPF_DashboardStateChanged_Event {
+    public partial class Dashboard1: DevExpress.DashboardCommon.Dashboard {
+        public Dashboard1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/WPF-DashboardStateChanged-Event/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWpf;
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace WPF_DashboardStateChanged_Event {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow: Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void DashboardControl_DashboardStateChanged(object sender, DashboardStateChangedWpfEventArgs e) {
+            var newState = e.DashboardState;
+            var message = string.Empty;
+            foreach(DashboardItemState itemState in newState.Items) {
+                var item = dashboardControl1.Dashboard.Items[itemState.ItemName];
+                message += item.Name;
+                if( itemState.DrillDownValues.Count != 0) {
+                    message += "\n" + "Filter Drill-Down:" + " " + string.Join("," , itemState.DrillDownValues);
+                }
+                if(itemState.MasterFilterValues.Count != 0) {
+                    message +=  "\n" + "Master Filter:" + " " + string.Join(" | ", itemState.MasterFilterValues.Select(v=> string.Join(",",v))); 
+                }
+                if (itemState.RangeFilterState.Selection.Minimum !=null || itemState.RangeFilterState.Selection.Maximum != null) {
+                    message += "\n" + "Range Filter:" + " " + ((DateTime)itemState.RangeFilterState.Selection.Minimum).ToString("y") + "-" + ((DateTime)itemState.RangeFilterState.Selection.Maximum).ToString("y");
+                }
+                message += Environment.NewLine;
+                message += "\n";
+            }
+                 TextEdit.Text = message;  
+        }            
+    }       
+}
+
+
+        
+    
+
